be9559ff625717b015c522768ee492b61a257f05
Nikola Derežić <nikola.derezic@ngit.hr>
2025-09-09 16:00:43 +0200

    Add build script for firmware compilation and flashing process

---
+++ tools/build.sh
@@ -0,0 +1,38 @@
+echo "Building firmware..."
+
+
+echo "Building LP (Low Power) project..."
+cd /workspace/project/realtek_amebaD_va0_example/GCC-RELEASE/project_lp
+make clean
+make all
+
+echo "Copying LP (Low Power) binaries to flash directory..."
+cp km0_boot_all.bin /workspace/flash/
+
+echo "Building HP (High Performance) project..."
+cd /workspace/project/realtek_amebaD_va0_example/GCC-RELEASE/project_hp/
+
+make clean
+make all
+
+echo "Copying HP (High Performance) binaries to flash directory..."
+cp km4_boot_all.bin /workspace/flash/
+cp km0_km4_image2.bin /workspace/flash/
+
+echo "Build process completed."
+echo "Do you want to flash the firmware to GW018-DM now? (y/n)"
+read -r response
+if [[ "$response" != "y" ]]; then
+    echo "Exiting without flashing."
+    exit 0
+fi
+
+echo "Flashing firmware to GW018-DM..."
+
+cd /workspace/flash
+
+# Erase flash before flashing new firmware (may not be needed)
+./upload_image_tool_linux "$PWD" /dev/ttyUSB0 ameba_rtl8721csm Enable Enable 921600
+
+# … and let's flash!
+./upload_image_tool_linux "$PWD" /dev/ttyUSB0 ameba_rtl8721csm Enable Disable 921600
